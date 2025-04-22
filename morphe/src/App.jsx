6b0afae3c1143b58e0642591c53da8e7024fe691
UdithKateel <udithshetty555@gmail.com>
2025-04-22 17:31:04 +0530

    sec

--- morphe/src/App.jsx
+++ morphe/src/App.jsx
@@ -6,19 +6,21 @@ import { Button, CloseButton, Drawer, Portal } from "@chakra-ui/react"
 import { IconButton } from '@chakra-ui/react'
 import { TiThMenuOutline } from "react-icons/ti"
 import Sidebar from './Sidebar'
+import Inputbar from './Inputbar'
+import { Heading } from '@chakra-ui/react'
 function App() {
   
     const [open, setOpen] = useState(false)
 
   return (
-   <Flex minH={'100dvh'} >
-    
-              <Drawer.Root placement={'start'} open={open} onOpenChange={(e) => setOpen(e.open)}>
+    <Flex minH="100vh" w="100%" direction="column" >
+  {/* Navbar */}
+  <Flex px="4" pt="4" align="center" justify="start" >
+    <Drawer.Root placement="start" open={open} onOpenChange={(e) => setOpen(e.open)}>
       <Drawer.Trigger asChild>
         <Button variant="outline" size="lg">
-              <TiThMenuOutline  />
-                Morpheus Agent
-             
+          <TiThMenuOutline />
+          Morpheus Agent
         </Button>
       </Drawer.Trigger>
       <Portal>
@@ -29,9 +31,8 @@ function App() {
               <Drawer.Title>Morpheus Agent</Drawer.Title>
             </Drawer.Header>
             <Drawer.Body>
-             <Sidebar/>
+              <Sidebar />
             </Drawer.Body>
-            
             <Drawer.CloseTrigger asChild>
               <CloseButton size="sm" />
             </Drawer.CloseTrigger>
@@ -39,16 +40,21 @@ function App() {
         </Drawer.Positioner>
       </Portal>
     </Drawer.Root>
-    <Box m={'auto'}>
-      <Stack h={'full'}>
-        <Box>
-          Top
-        </Box>
-          <Center flex={'1'}>Middle</Center>
-          <Box pb={'18px'}>Bottom</Box>
-      </Stack>
-    </Box>
-   </Flex>
+  </Flex>
+
+  {/* Main content vertically and horizontally centered */}
+  <Flex flex="1" justify="center" align="center"  minH="calc(100vh - 100px)" >
+    <Stack align="center" spacing={4} w="full" mb={10} >
+      <Heading size="3xl" p={'4px'} color="white" textAlign="center">
+        What can I do for you Today?
+      </Heading>
+      <Box w="full" maxW="600px">
+        <Inputbar />
+      </Box>
+    </Stack>
+  </Flex>
+</Flex>
+
   )
 }
 
---
+++ morphe/src/Inputbar.jsx
@@ -0,0 +1,52 @@
+import { Box, Field, Textarea, defineStyle } from "@chakra-ui/react";
+
+const Inputbar = () => {
+  return (
+    <Field.Root>
+      <Box pos="relative" w="full">
+        <Textarea
+          className="peer"
+          placeholder=""
+          resize="vertical" // Allow vertical resizing
+          overflowY="auto"
+          lineHeight="1.5"
+          pr="4"
+          css={{
+            scrollbarWidth: "thin",
+            "&::-webkit-scrollbar": {
+              width: "6px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              background: "#A0AEC0",
+              borderRadius: "6px",
+            },
+          }}
+        />
+        <Field.Label css={floatingStyles}>Task is ..</Field.Label>
+      </Box>
+    </Field.Root>
+  );
+};
+
+const floatingStyles = defineStyle({
+  pos: "absolute",
+  bg: "bg",
+  px: "0.5",
+  top: "-3",
+  insetStart: "2",
+  fontWeight: "normal",
+  pointerEvents: "none",
+  transition: "position",
+  _peerPlaceholderShown: {
+    color: "fg.muted",
+    top: "2.5",
+    insetStart: "3",
+  },
+  _peerFocusVisible: {
+    color: "fg",
+    top: "-3",
+    insetStart: "2",
+  },
+});
+
+export default Inputbar;
